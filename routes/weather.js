e274c0bca2a27b357bf8300153c904f6462e0879
Francois Guthmann (Jarvis) <francois.guthmann@telecom-paristech.fr>
2017-05-30 22:05:13 +0200

    Renvoie des coords lat/long

--- routes/weather.js
+++ routes/weather.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var weatherService = require('../server/weatherService');
+var parser = require('json-parser');
 
 /* GET hello world page. */
 router.get('/', function(req, res, next) {
 	weatherService.getGeolocalisation("Brest").then(function(data){
-		res.send(data);
+		var parsedData = parser.parse(data);
+		var coords = parsedData.results[0].geometry.location;
+		console.log(coords);
+		res.send("lat: " + coords.lat + " and long: " + coords.lng);
 	});
 });
 
